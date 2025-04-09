2d48117e6f49734408f66f9b84778933f16fd897
kky1225 <kky971225@naver.com>
2025-04-09 21:46:55 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/example/investfeed/InvestFeedApplication.kt
@@ -0,0 +1,11 @@
+package com.example.investfeed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvestFeedApplication
+
+fun main(args: Array<String>) {
+    runApplication<InvestFeedApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/investfeed/InvestFeedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.investfeed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvestFeedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
