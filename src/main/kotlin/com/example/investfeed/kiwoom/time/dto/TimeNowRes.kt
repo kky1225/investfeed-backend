3a58373aef81d1a09252703d412dc89b3c19ff0f
kky1225 <kky971225@naver.com>
2025-09-03 21:36:42 +0900

    임시커밋

--- src/main/kotlin/com/example/investfeed/kiwoom/time/dto/TimeNowRes.kt
+++ src/main/kotlin/com/example/investfeed/kiwoom/time/dto/TimeNowRes.kt
@@ -1,5 +1,9 @@
 package com.example.investfeed.kiwoom.time.dto
 
 data class TimeNowRes(
-    var time: Long
+    var time: Long,
+    var isMarketOpen: Boolean,
+    var startMarketTime: Long,
+    var endMarketTime: Long,
+    var marketType: String? = null,
 )
--- src/main/kotlin/com/example/investfeed/kiwoom/time/service/TimeService.kt
+++ src/main/kotlin/com/example/investfeed/kiwoom/time/service/TimeService.kt
@@ -1,13 +1,48 @@
 package com.example.investfeed.kiwoom.time.service
 
+import com.example.investfeed.kiwoom.config.MarketType
 import com.example.investfeed.kiwoom.time.dto.TimeNowRes
 import org.springframework.stereotype.Service
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneId
 
 @Service
 class TimeService() {
     fun timeNow(): TimeNowRes {
+        val now = System.currentTimeMillis()
+        var nowEpoch = Instant.ofEpochMilli(now).atZone(ZoneId.of("Asia/Seoul"))
+        val time = nowEpoch.toLocalTime()
+        var marketType: String? = null
+        var isMarketOpen = false
+
+        if (time.isAfter(LocalTime.of(9, 0)) && time.isBefore(LocalTime.of(15, 30))) {
+            marketType = MarketType.KRX.name
+            isMarketOpen = true
+        } else if (time.isAfter(LocalTime.of(8, 0)) && time.isBefore(LocalTime.of(20, 0))) {
+            marketType = MarketType.NXT.name
+            isMarketOpen = true
+        } else if(time.isAfter(LocalTime.of(20, 30))) {
+            nowEpoch = nowEpoch.plusDays(1)
+        }
+
+        val startMarketTime = LocalDateTime.of(nowEpoch.toLocalDate(), LocalTime.of(9, 0))
+            .atZone(ZoneId.of("Asia/Seoul"))
+            .toInstant()
+            .toEpochMilli()
+
+        val endMarketTime = LocalDateTime.of(nowEpoch.toLocalDate(), LocalTime.of(20, 0))
+            .atZone(ZoneId.of("Asia/Seoul"))
+            .toInstant()
+            .toEpochMilli()
+
         return TimeNowRes(
-            time = System.currentTimeMillis(),
+            time = now,
+            marketType = marketType,
+            startMarketTime = startMarketTime,
+            endMarketTime = endMarketTime,
+            isMarketOpen = isMarketOpen,
         )
     }
 }
